386fbe34caaa72a2fbb2281977bbc767c3bfdda7
dev-web-jakekim <dev-web-jakekim@gmail.com>
2023-06-27 23:50:43 +0900

    added main menu

--- js/main.js
+++ js/main.js
@@ -14,3 +14,21 @@ searchInputEl.addEventListener("blur", function () {
   searchInputEl.classList.remove("focused");
   searchInputEl.setAttribute("placeholder", "");
 });
+
+
+const badgeEl = document.querySelector('header .badges');
+
+window.addEventListener('scroll', _.throttle(function () {
+  console.log('scrill')
+  if (window.scrollY > 500) {
+    gsap.to(badgeEl, .6, {
+      opacity: 0,
+      display: 'none'
+    });
+  } else {
+    gsap.to(badgeEl, .6, {
+      opacity: 1,
+      display: 'block'
+    });
+  }
+}, 300));
